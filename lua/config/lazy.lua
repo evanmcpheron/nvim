ca51519b9e8f848ce2e5e60019a5e770b20a0a3f
Evan McPheron <evan.mcpheron@icloud.com>
2024-10-28 10:05:52 -0700

    adds relative line numbers

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -3,13 +3,20 @@ local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
   -- bootstrap lazy.nvim
   -- stylua: ignore
-  vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable", lazypath })
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable",
+    lazypath,
+  })
 end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
   spec = {
-    -- add LazyVim and import its plugins
+    -- Add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
     {
       "neovim/nvim-lspconfig",
@@ -19,10 +26,34 @@ require("lazy").setup({
         opts.servers.tsserver = {
           on_attach = function(client, bufnr)
             local opts = { noremap = true, silent = true }
-            vim.api.nvim_buf_set_keymap(bufnr, "n", "gd", "<Cmd>lua vim.lsp.buf.definition()<CR>", opts)
-            vim.api.nvim_buf_set_keymap(bufnr, "n", "K", "<Cmd>lua vim.lsp.buf.hover()<CR>", opts)
-            vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>rn", "<Cmd>lua vim.lsp.buf.rename()<CR>", opts)
-            vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>f", "<Cmd>lua vim.lsp.buf.format { async = true }<CR>", opts)
+            vim.api.nvim_buf_set_keymap(
+              bufnr,
+              "n",
+              "gd",
+              "<Cmd>lua vim.lsp.buf.definition()<CR>",
+              opts
+            )
+            vim.api.nvim_buf_set_keymap(
+              bufnr,
+              "n",
+              "K",
+              "<Cmd>lua vim.lsp.buf.hover()<CR>",
+              opts
+            )
+            vim.api.nvim_buf_set_keymap(
+              bufnr,
+              "n",
+              "<leader>rn",
+              "<Cmd>lua vim.lsp.buf.rename()<CR>",
+              opts
+            )
+            vim.api.nvim_buf_set_keymap(
+              bufnr,
+              "n",
+              "<leader>f",
+              "<Cmd>lua vim.lsp.buf.format { async = true }<CR>",
+              opts
+            )
           end,
           flags = {
             debounce_text_changes = 150,
@@ -50,25 +81,40 @@ require("lazy").setup({
     },
     { import = "lazyvim.plugins.extras.linting.eslint" },
     { import = "lazyvim.plugins.extras.formatting.prettier" },
+
+    -- Configure neo-tree to show line numbers and hidden files
+    {
+      "nvim-neo-tree/neo-tree.nvim",
+      opts = {
+        filesystem = {
+          filtered_items = {
+            visible = true,          -- Show all items, including hidden files
+            hide_dotfiles = false,   -- Do not hide dotfiles
+            hide_gitignored = false, -- Do not hide gitignored files
+          },
+        },
+        event_handlers = {
+          {
+            event = "neo_tree_buffer_enter",
+            handler = function(arg)
+              vim.cmd [[
+                setlocal relativenumber
+              ]]
+            end,
+          }
+        },
+      },
+    },
   },
   defaults = {
-    -- By default, only LazyVim plugins will be lazy-loaded. Your custom plugins will load during startup.
-    -- If you know what you're doing, you can set this to `true` to have all your custom plugins lazy-loaded by default.
     lazy = false,
-    -- It's recommended to leave version=false for now, since a lot the plugin that support versioning,
-    -- have outdated releases, which may break your Neovim install.
-    version = false, -- always use the latest git commit
-    -- version = "*", -- try installing the latest stable version for plugins that support semver
+    version = false,
   },
-  checker = { enabled = true }, -- automatically check for plugin updates
+  checker = { enabled = true },
   performance = {
     rtp = {
-      -- disable some rtp plugins
       disabled_plugins = {
         "gzip",
-        -- "matchit",
-        -- "matchparen",
-        -- "netrwPlugin",
         "tarPlugin",
         "tohtml",
         "tutor",
